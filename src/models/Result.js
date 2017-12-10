31a92f9e24783185ed2cd9e9498f03d1fd090e78
Samb101 <samuelbenais@outlook.com>
2017-12-10 18:25:29 +0100

    yoyo

--- src/models/Result.js
+++ src/models/Result.js
@@ -1,13 +1,11 @@
 export default class Result {
     constructor(result) {
-        console.log(result)
         this.artists = result.data.artists;
         this.title = result.data.title;
         this.available = result.data.available;
         this.price = result.data.price;
         this.match = result.data.match;
         this.url = result.data.url;
-        console.log(this);
     }
 
     artists;
